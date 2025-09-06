74b1bb66470a142b561aaca9e62ffc2f9b3979ce
spapiernik <spapiernik12@gmail.com>
2025-09-06 13:33:35 -0500

    chore: add build/ directory generated during failing Windows build

---
+++ build/conanrunenv-release-x86_64.bat
@@ -0,0 +1,22 @@
+@echo off
+chcp 65001 > nul
+setlocal
+echo @echo off > "%~dp0/deactivate_conanrunenv-release-x86_64.bat"
+echo echo Restoring environment >> "%~dp0/deactivate_conanrunenv-release-x86_64.bat"
+for %%v in (QT_PLUGIN_PATH OPENSSL_MODULES) do (
+    set foundenvvar=
+    for /f "delims== tokens=1,2" %%a in ('set') do (
+        if /I "%%a" == "%%v" (
+            echo set "%%a=%%b">> "%~dp0/deactivate_conanrunenv-release-x86_64.bat"
+            set foundenvvar=1
+        )
+    )
+    if not defined foundenvvar (
+        echo set %%v=>> "%~dp0/deactivate_conanrunenv-release-x86_64.bat"
+    )
+)
+endlocal
+
+
+set "QT_PLUGIN_PATH=C:\Users\spapi\.conan2\p\qt587937845c546\p\plugins"
+set "OPENSSL_MODULES=C:\Users\spapi\.conan2\p\opens53b2260921422\p\lib\ossl-modules"
